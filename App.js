782222d9bd1605bbf67a164778c2126bb5a54b13
Kgaotlhaelwe <Kgaotlhaelwe@gmail.com>
2023-09-30 15:10:19 +0200

    login

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View,Image } from "react-native";
+import Welcome from './pages/welcome';
+import Login from './pages/login'
+import SignUp from './pages/signUp';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SignUp />
     </View>
   );
 }
